814f5ba62ce4fb570901b09fc929c43933d7aa69
Tom Wilson <twilson63@gmail.com>
2020-10-21 11:39:50 -0400

    switching site to sapper

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import App from './App.svelte'
-
-export default new App({target: document.body, props: {
-  sku: 'prod_I73oYiiLUL8myW'
-}})
-
-
---
+++ src/routes/workshops/index.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<style>
+	ul {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+</style>
+
+<svelte:head>
+	<title>hyper63 - Workshops</title>
+</svelte:head>
+
+<h1>Upcoming Workshops</h1>
+
+<ul>
+  <li><a rel="prefetch" href="workshops/svelte-fundamentals">Svelte Fundamentals</a></li>
+  
+</ul>
