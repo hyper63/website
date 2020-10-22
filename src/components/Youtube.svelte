8fe882f3ab60466a2494423e24672462cc4de9a3
Tom Wilson <twilson63@gmail.com>
2020-10-22 10:47:52 -0400

    created a youtube component

---
+++ src/components/Youtube.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let video
+</script>
+<object width="425" height="300" title="youtube video">
+  <param name="movie" value="https://www.youtube.com/v/{video}&hl=en&fs=1" />
+  <param name="allowFullScreen" value="true" />
+  <embed src="https://www.youtube.com/v/{video}&hl=en&fs=1" type="application/x-shockwave-flash" allowfullscreen="true" width="425" height="300" />
+</object>
