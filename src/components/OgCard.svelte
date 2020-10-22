cf216895ba2f96b0f3be0ba0ac1eefad0e6159f8
Tom Wilson <twilson63@gmail.com>
2020-10-22 09:58:02 -0400

    include og card

---
+++ src/components/OgCard.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let url, title, description, image
+</script>
+<meta property="og:type" content="website">
+<meta property="og:url" content="{url}">
+<meta property="og:title" content="{title}">
+<meta property="og:description" content="{description}">
+<meta property="og:image" content="{image}">
